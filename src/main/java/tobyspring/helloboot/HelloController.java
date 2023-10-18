4e82e0960647671f578eb443445cafd812336def
golddrone7 <golddrone7@naver.com>
2023-10-18 20:45:48 +0900

    get

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,6 +3,7 @@ package tobyspring.helloboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// Rest 방식을 이용 응답을 바디에 특정한 타입을 인코딩해서 하는 방식
 @RestController
 public class HelloController {
     private final HelloService helloService;
@@ -11,11 +12,18 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    // http 메서드가 GET으로만 되어있는 것만 받겠다.
     @GetMapping("/hello")
-    public String hello(String name) {
+    public String hello(String name) {  // 메서드 파라미터를 지정하면 전달함
         if (name == null || name.trim().length() == 0) throw new IllegalArgumentException();
 
         return helloService.sayHello(name);
+    }
+
+    @GetMapping("/bye")
+    public String bye(){
+        System.out.println("ㅎㅇㅎㅇ");
+        return "Hello ";
     }
 
     @GetMapping("/count")
