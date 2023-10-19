8656022d64c0998c729d7705dbc243ac2e3fed5d
golddrone <golddrone@naver.com>
2023-10-19 14:00:16 +0900

    프론트 컨트롤러로 전환

--- src/main/java/tobyspring/study/StudyApplication.java
+++ src/main/java/tobyspring/study/StudyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -23,17 +24,28 @@ public class StudyApplication {
 
         // Jetty, Tomcat 추상화
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("study", new HttpServlet(){
+            servletContext.addServlet("frontcontroller", new HttpServlet(){
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-                    String name = req.getParameter("name");
-                    // 상태코드, 컨텐츠타입 header, Body
-                    // 200 정상
-                    resp.setStatus(HttpStatus.OK.value());
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                    resp.getWriter().println("Hellos " + name);
+                    // 인증, 보안, 다국어, 공통 기능
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
+                        // 상태코드, 컨텐츠타입 header, Body
+                        // 200 정상
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println("Hellos " + name);
+
+                    }
+                    else if (req.getRequestURI().equals("/user")){
+
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
+
+
                 }
-            }).addMapping("/study");
+            }).addMapping("/*"); // 모든 요청을 다 받겠다. FrontController 책임을 맡음
         });// 웹 서버 생성함수
         // 톰캣이 실행
 
