a350d1f95f6180e3db882ba1cc6d9dbacdd7481e
golddrone <golddrone@naver.com>
2023-10-19 13:50:30 +0900

    서블릿 요청 처리

--- src/main/java/tobyspring/study/StudyApplication.java
+++ src/main/java/tobyspring/study/StudyApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -23,11 +26,12 @@ public class StudyApplication {
             servletContext.addServlet("study", new HttpServlet(){
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+                    String name = req.getParameter("name");
                     // 상태코드, 컨텐츠타입 header, Body
                     // 200 정상
-                    resp.setStatus(200);
-                    resp.setHeader("Content-Type", "text/plain");
-                    resp.getWriter().println("Hello Servlet");
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("Hellos " + name);
                 }
             }).addMapping("/study");
         });// 웹 서버 생성함수
